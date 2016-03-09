1c8e2727f7db48ac4bdbbb23959cd1c903b24544
jai1396 <jai1396@gmail.com>
2016-03-09 14:52:44 +0530

    easysearch1stimpl

---
+++ client/Partials/Homepage.js
@@ -0,0 +1,64 @@
+if(Meteor.isClient)
+{
+	// Template.searchboard.player =function(){
+	// 	return "\nSome other text"
+	// }
+	
+	Template.searchboard.helpers({
+		inputAttributes: function () {
+	      return { 'class': 'easy-search-input', 'placeholder': 'Start searching...' };
+	    },
+		'uni': function(){
+			Meteor.subscribe('universities');
+			return Universities.find({}, {sort: {name: 1}})
+		},
+		// 'selectedClass': function(){
+		// 	var playerID = this._id;
+		// 	var selectedPlayer = Session.get('selectedPlayer');
+		// 	if(selectedPlayer == playerID){
+		// 		return "selected"
+		// 	}	
+					
+		// },
+		// 'showSelectedPlayer': function(){
+		// 	var selectedPlayer = Session.get('selectedPlayer');
+		// 	return Universities.findOne(selectedPlayer);
+		// },
+		index: function () {
+	    	return UniIndex;
+		},
+		'selectedUni': function(){
+			// var selectedUni = Session.get('selectedUni');
+			// return Universities.findOne(selectedUni);
+			return Session.equals("selectedUni", this.__originalId) ? "selected" : '';
+		},
+		// selectedName: function () {
+	 //      var player = UniIndex.config.mongoCollection.findOne({ __originalId: Session.get("selectedPlayer") });
+	 //      return player && player.name;
+	 //    },
+	});
+
+	Template.searchboard.events({
+		'click .uni': function(){
+			var uniID = this._id;
+			Session.set('selectedUni', uniID);
+		}
+	});
+
+	Template.uni.helpers({
+    selected: function () {
+      return Session.equals("selectedUni", this.__originalId) ? "selected" : '';
+   //    var selectedUni = Session.get('selectedUni');
+			// return Universities.findOne(selectedUni);
+    }
+  });
+
+  Template.uni.events({
+    'click': function () {
+      Session.set("selectedUni", this.__originalId);
+    }
+  });
+
+	
+
+}
--- collections/University.js
+++ collections/University.js
@@ -1,4 +1,36 @@
 Universities = new Mongo.Collection('universities');
+<<<<<<< Updated upstream
+=======
+
+UniIndex = new EasySearch.Index({
+  engine: new EasySearch.MongoDB({
+    sort: function () {
+      return { name: 1 };
+    },
+    selector: function (searchObject, options, aggregation) {
+      let selector = this.defaultConfiguration().selector(searchObject, options, aggregation),
+        categoryFilter = options.search.props.categoryFilter;
+
+      if (_.isString(categoryFilter) && !_.isEmpty(categoryFilter)) {
+        selector.category = categoryFilter;
+      }
+
+      return selector;
+    }
+  }),
+  collection: Universities,
+  fields: ['name'],
+  defaultSearchOptions: {
+    limit: 8
+  },
+  permission: () => {
+    //console.log(Meteor.userId());
+
+    return true;
+  }
+});
+
+>>>>>>> Stashed changes
 opt = {};
 
 opt.phd = {
