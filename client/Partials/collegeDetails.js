16ead4de4cb1bfd21844d8062942d8b2058702b8
Jaidev Anoop <jaidev.anoop@gmail.com>
2016-03-14 01:39:53 +0530

    Home Changes

--- client/Partials/collegeDetails.js
+++ client/Partials/collegeDetails.js
@@ -22,6 +22,7 @@ Template.collegeDetails.events({
 
 Template.moreCollegeDetails.helpers({
 	'college': function(){
+		Meteor.subscribe('universities');
 		var id = Session.get('collegeSelected');
 		return Universities.findOne(id);
 	}
