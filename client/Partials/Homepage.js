d72492a2db79f8f42f367d3eac9494fa012dd1ea
vidit23 <viditvineet@hotmail.com>
2016-03-30 01:48:40 +0530

    Slider

--- client/Partials/Homepage.js
+++ client/Partials/Homepage.js
@@ -6,6 +6,61 @@ Template.Search.helpers({
 	}
 });
 
+Template.AdvSearch.helpers({
+	slider: function() {
+		return Session.get("slider");
+	},
+	filterGRE: function() {
+		Meteor.subscribe('universities');
+		var value1 = Session.get('filterHighGRE');
+		var value2 = Session.get('filterLowGRE');
+		return Universities.find({greCutoff: {$gt: value2, $lt: value1}}, {sort: {greCutoff: 1}});
+	}
+
+});
+
+Template.AdvSearch.rendered = function () {
+	var range = document.getElementById('slider');
+
+	noUiSlider.create(range, {
+		start: [ 280, 310 ], // Handle start position
+		step: 1, // Slider moves in increments of '1'
+		margin: 10, // Handles must be more than '10' apart
+		connect: true, // Display a colored bar between the handles
+		//direction: 'rtl', // Put '0' at the bottom of the slider
+		//orientation: 'vertical', // Orient the slider vertically
+		behaviour: 'tap-drag', // Move handle on tap, bar is draggable
+		range: { // Slider can select '0' to '100'
+		'min': 260,
+		'max': 340
+	},
+	});
+
+	var valueLow = document.getElementById('value-input-low'),
+		valueHigh = document.getElementById('value-input-high');
+
+		// When the slider value changes, update the input and span
+	range.noUiSlider.on('update', function( values, handle ) {
+		if ( handle ) {
+			valueHigh.value = values[handle];
+			Session.set('filterHighGRE', values[handle]);
+		} else {
+			valueLow.value = values[handle];
+			Session.set('filterLowGRE', values[handle]);
+		}
+	});
+
+	// When the input changes, set the slider value
+	valueHigh.addEventListener('change', function(){
+		range.noUiSlider.set([null, this.value]);
+		Session.set('filterHighGRE', this.value);
+	});
+	valueLow.addEventListener('change', function(){
+		range.noUiSlider.set([this.value, null]);
+		Session.set('filterLowGRE', this.value);
+	});
+};
+
 Template.Homepage.events({
 	'click #us': function(){
 		Router.go('/search');
@@ -25,8 +80,8 @@ Template.searchboard.helpers({
 	// 	var selectedPlayer = Session.get('selectedPlayer');
 	// 	if(selectedPlayer == playerID){
 	// 		return "selected"
-	// 	}	
-				
+	// 	}
+
 	// },
 	// 'showSelectedPlayer': function(){
 	// 	var selectedPlayer = Session.get('selectedPlayer');
