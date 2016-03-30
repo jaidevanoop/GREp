53d8d75f870aa968b06c2c6bf55115a34014aea2
vidit23 <viditvineet@hotmail.com>
2016-03-30 14:52:09 +0530

    Some shit

--- client/Partials/collegeDetails.js
+++ client/Partials/collegeDetails.js
@@ -1,25 +1,3 @@
-Template.collegeDetails.helpers({
-	'college': function(){
-		Meteor.subscribe('universities');
-
-		return Universities.find({});
-	},
-	'selected': function(){
-		var id = Session.get('collegeSelected');
-		if(this._id == id)
-			return "active"
-	}
-
-});
-
-Template.collegeDetails.events({
-	'click .list-group-item': function(){
-		Session.set('collegeSelected',this._id);
-	}
-
-
-});
-
 Template.moreCollegeDetails.helpers({
 	'college': function(){
 		Meteor.subscribe('universities');
