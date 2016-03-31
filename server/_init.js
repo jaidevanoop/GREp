f5d4d7ad5b3e81b2bd7d27811ccf936064160a09
vidit23 <viditvineet@hotmail.com>
2016-03-31 17:26:54 +0530

    Fixed confirm mail

--- server/_init.js
+++ server/_init.js
@@ -1,12 +1,12 @@
 Meteor.startup(function() {
-	// process.env.MAIL_URL='smtp://postmaster%40sandboxf5e3c6590fc2418788443ae68858b9a9.mailgun.org:01a9bfb2ae4a7595597c23a18ece90fb@smtp.mailgun.org:587';
+	process.env.MAIL_URL='smtp://postmaster%40sandboxf5e3c6590fc2418788443ae68858b9a9.mailgun.org:01a9bfb2ae4a7595597c23a18ece90fb@smtp.mailgun.org:587';
 	//
-	// Accounts.emailTemplates.from = 'no-reply@yourdomain.com';
-	// Accounts.emailTemplates.sitename = 'GREp';
-	//
-	// Accounts.emailTemplates.verifyEmail.subject = function(user){
-	// 	return 'Confirm Your Email Address';
-	// };
+	Accounts.emailTemplates.from = 'no-reply@yourdomain.com';
+	Accounts.emailTemplates.sitename = 'GREp';
+
+	Accounts.emailTemplates.verifyEmail.subject = function(user){
+	 	return 'GREp: Confirm Your Email Address';
+	};
 	// Accounts.emailTemplates.verifyEmail.text = function(user,url){
 	// 	return 'Click on the following link to verify your email address: ' + url;
 	// };
