3355f66ef52af3c2cc337340ca3c9550dd6782d7
Jaidev Anoop <jaidev.anoop@gmail.com>
2016-04-05 19:50:14 +0530

    Bookmark done

--- client/Partials/bookmark.js
+++ client/Partials/bookmark.js
@@ -1,15 +1,20 @@
-// Template.bookmark.helpers({
-// 	'obj': function() {
-// 		console.log(Meteor.userId());
-// 		return Universities.find({bookmark:{$in:[this.params._id]}});
-// 	}
-// });
-
 Template.bookmark.helpers({
 	'bookmark': function() {
 		Meteor.subscribe('universities');
 		var id = Meteor.userId();
-		console.log(id);
-		return Universities.find({bookmark:{$in:[id]}});
+		return Universities.find({bookmark:{$in:[id]}},{sort:{greCutoff:-1,toeflCutoff:-1}});
+	},
+	'details': function(){
+		if(Session.get('selectedUni'))
+			return true;
+		return false;
+	}
+});
+
+Template.bookmark.events({
+	'click .remove': function() {
+		var id = this._id;
+		var user_id = Meteor.userId();
+		Universities.update({_id: id},{$pull: { bookmark: user_id}});
 	}
 });
